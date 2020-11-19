df4c91b94694a049428f84b7c8c1840c460b75bf
Christopher <Christopher@DESKTOP-PE979GK>
2020-11-19 22:18:28 +0100

    Aggiungere i file di progetto.

---
+++ ChatbotServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChatbotServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
